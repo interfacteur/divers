393be8531b90fef7d3a81fa61fb6a4cb99131505
interfacteur <interfacteur@users.noreply.github.com>
2014-06-24 12:33:38 +0200

    Improve organization and comments

--- colorToContrastedColor.js
+++ colorToContrastedColor.js
@@ -1,33 +1,34 @@
+ /*
+To find an enhaced contrast
 
-/**
- * Provide contrasted values of a RGB color via its HLS values in order to extract complemtary colors
- *
- * RGB to HSL and HSL to RGB adapted from http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
- * wich gives all conversion formulas
- * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes r, g, and b are contained in the set [0, 255]
- *
- * @param   Number  r       The red color value
- * @param   Number  g       The green color value
- * @param   Number  b       The blue color value
- * @return  Array           The RGB representation of a contrasted value of the original RGB color, and the WCAG 2 contrast ratio
-**/
+@return		Array	["RGB string for contrasted color",floating number for contrast ratio]
 
-var toContrast = function(origR,origG,origB) {
-	"use strict";
+Example: http://www.equatorium.net/e1/ou-obtenir-contraste_en.html
 
-	//axonflux.com : rgb to h (s and l are declined in an array)
+Functions converting rgb to hsl and hsl to rgb
+are from
+http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+and also parameters:
+ * @param	Number	origR		The red color value
+ * @param	Number	origG		The green color value
+ * @param	Number	origB		The blue color value
+ */
+
+var contrasteTrouver = function(origR,origG,origB) {
+
 	var r = origR / 255,
 		g = origG / 255,
 		b = origB / 255,
 		max = Math.max(r, g, b),
 		min = Math.min(r, g, b),
-		h, s, l,
-		contraste = [0];
+		h, s, l = (max + min) / 2,
+		contrasteOK = ["",0];
 
-	if(max == min) h = 0; // achromatic
+	//from axonflux.com : rgb to h (s and l are declined in an array)
+	if(max == min) h = s = 0; // achromatic
 	else{
 		var d = max - min;
+		s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
 		switch(max){
 			case r: h = (g - b) / d + (g < b ? 6 : 0); break;
 			case g: h = (b - r) / d + 2; break;
@@ -35,32 +36,63 @@ var toContrast = function(origR,origG,origB) {
 		}
 		h /= 6;
 	}
+	//end from axonflux.com : rgb to h (s and l are declined in an array)
 
-	//roue chromatique : les complementaires (http://scrap2sam.canalblog.com/archives/2010/04/18/17610481.html)
-	var H = [(h + .25) % 1,(h + .5) % 1,(h + .75) % 1]; 
+	if (l < .15 || l > .85){
+		var rgb = hsl2rgb(h,s,1 - l);
+	//return 1
+		return contrasteOK = [
+			Math.floor(rgb[0] * 255) + "," + Math.floor(rgb[1] * 255) + "," + Math.floor(rgb[2] * 255),
+			Math.floor(contrasteEvaluer(origR,origG,origB,rgb[0] * 255,rgb[1] * 255,rgb[2] * 255) * 100) / 100
+		];
+	}
+	//Possible values of hue from 5 complementary colors
+	var H = [
+				(h + .25) % 1,
+				(h + .33) % 1,
+				(h + .5) % 1,
+				(h + .66) % 1,
+				(h + .75) % 1
+			];
+
 	H.forEach(function(zv1) {
 		sl.forEach(function(zv2) {
-
-			//axonflux.com : hsl to rgb
-			h = zv1, s = zv2[0], l = zv2[1];
-			if(s == 0) r = g = b = l; // achromatic
-			else{
-				var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-				var p = 2 * l - q;
-				r = hue2rgb(p, q, h + 1/3);
-				g = hue2rgb(p, q, h);
-				b = hue2rgb(p, q, h - 1/3);
-			}
-
-			//le contraste le plus accentue
-			var contrastant = contrasteEvaluer(origR,origG,origB,r * 255,g * 255,b * 255);
-			if (contraste[0] < contrastant) contraste = [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255), contrastant];
-	});	});
-
-	return contraste;
+			var rgb = hsl2rgb(zv1,zv2[0],zv2[1]);
+			//the more ehanced contrast
+			var contrastant = contrasteEvaluer(origR,origG,origB,rgb[0] * 255,rgb[1] * 255,rgb[2] * 255);
+			if (contrasteOK[1] < contrastant){
+				contrasteOK = [
+					Math.floor(rgb[0] * 255) + "," + Math.floor(rgb[1] * 255) + "," + Math.floor(rgb[2] * 255),
+					Math.floor(contrastant * 100) / 100
+				];
+	}	});	});
+	//final return
+	return contrasteOK;
 }
 
-//axonflux.com : method
+//Possible values of saturation and luminosity
+var sl = [];
+[0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1].forEach(function(zv1) {
+	[.05,.1,.2,.3,.4,.5,.6,.7,.8,.9,.95].forEach(function(zv2) {
+		sl.push([zv1,zv2]);
+});	});
+
+//axonflux.com : hsl to rgb
+function hsl2rgb(h,s,l) {
+	var r, g, b;
+	if(s == 0) r = g = b = l; // achromatic
+	else{
+		var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		var p = 2 * l - q;
+		r = hue2rgb(p, q, h + 1/3);
+		g = hue2rgb(p, q, h);
+		b = hue2rgb(p, q, h - 1/3);
+	}
+	return [r,g,b];
+}
+//end of axonflux.com : hsl to rgb
+
+//axonflux.com : method for hsl to rgb
 function hue2rgb(p, q, t) {
 	if(t < 0) t += 1;
 	if(t > 1) t -= 1;
@@ -69,16 +101,10 @@ function hue2rgb(p, q, t) {
 	if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
 	return p;
 }
+//end of axonflux.com : method for hsl to rgb
 
-var sl = [];
-[0,.125,.25,.375,.5,.625,.75,.875,1].forEach(function(zv1) {
-	[.125,.25,.375,.5,.625,.75,.875].forEach(function(zv2) {
-		sl.push([zv1,zv2]);
-});	});
-
-//calculer les contrastes
-//http://www.w3.org/TR/WCAG20-TECHS/G17.html
-//http://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-contrast.html
+//To calculate contrast ratio from WCAG 2: http://www.w3.org/TR/WCAG20-TECHS/G17.html
+/* seel also https://github.com/interfacteur/js-astuces/blob/master/colorContrastCalculation.js */
 function hexaTraiter(n) {
 	n /= 255;
 	return n < 0.03928 ? n / 12.92 : Math.pow(((n + 0.055) / 1.055), 2.4);
@@ -91,4 +117,3 @@ function contrasteEvaluer(r,g,b,R,G,B) {
 		L2 = luminanceCalculer(R,G,B);
 	return L1 > L2 ? (L1 + 0.05) / (L2 + 0.05) : (L2 + 0.05) / (L1 + 0.05);
 }
- 
