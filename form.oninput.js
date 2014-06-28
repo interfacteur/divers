4dfe98be3013da0c90539278e09835ee245184cc
interfacteur <interfacteur@users.noreply.github.com>
2014-06-28 17:09:50 +0200

    comments

--- form.oninput.js
+++ form.oninput.js
@@ -1,6 +1,6 @@
 /* To manage 'oninput' and its fallback ('onkeydown' + 'onkeyup')
 
-Note: for HTML5 inputs without 'oninput' event support, like 'range' on MSIE????:
+Note: for HTML5 inputs without 'oninput' event support, like 'range' on MSIE (???? tests on virtual machcine):
 el.oninput = function() {
 	this.onchange = null;
 	faire();
