966a996e626c5640729649f752796c9149b48138
Interfacteur <gaetan@interfacteur.net>
2015-03-23 08:47:39 +0100

    sound

--- son.js
+++ son.js
@@ -1,16 +1,26 @@
-var commonLAg = {
+//use of sound HTML5 API - with .mp3 and .ogg
+
+//Gaëtan Langhade, Interfacteur, 2015 march
+
+
+/* to use :
+	var sounds = { k: path.without.extension, k: etc.}
+	globMeth.Sound.init(sounds)
+	globMeth.sounds["k"].play() where "k" is the key of the sound path in object parameter sd
+*/
+
+
+var globMeth = {
 	doNothing: function () { }
 };
+
 
 // $(function () {
 	// "use strict";
 
-	var audioCompat;
-
-//Pseudo-class Sound ----------------------------------------------------------------------------
-	//to use : commonLAg.sounds["k"].play()
-	commonLAg.Sound = commonLAg.doNothing;
-	(	(audioCompat = (function () {
+/* Pseudo-class Sound */
+	globMeth.Sound = globMeth.doNothing;
+	(	(globMeth.audioCompat = (function () {
 			"use strict";
 			var audio = document.createElement("audio")
 			return !! audio.canPlayType ?
@@ -18,36 +28,34 @@ var commonLAg = {
 						".mp3" : audio.canPlayType('audio/ogg; codecs="vorbis"') ? ".ogg" : false	)
 				: false;
 		})())
-	//ok for the class (with mp3 and ogg)
-		&& (commonLAg.Sound = function (source) {
+	//fine for the class (with mp3 and ogg)
+		&& (globMeth.Sound = function (srce) {
 			"use strict";
-			if (! this instanceof commonLAg.Sound)
+			if (! this instanceof globMeth.Sound)
 				throw new Error("Attention à l'instanciation");
-			// this.source = source + commonLAg.Sound.format();
-			this.audio = new Audio(source + audioCompat);
+			// this.srce = srce + globMeth.audioCompat;
+			// this.ikey = p; //with one parameter more, keys of object of sounds path in globMeth.Sound.init(): new globMeth.Sound(sd[p], p) and globMeth.Sound = function (srce, p)
+			this.audio = new Audio(srce + globMeth.audioCompat);
 		})
-		&& (commonLAg.Sound.prototype.play = function () {
+		&& (globMeth.Sound.prototype.play = function () {
 			"use strict";
 			this.audio.play();
 			return true;
 		})
-		&& (commonLAg.Sound.prototype.pause = function () {
+		&& (globMeth.Sound.prototype.pause = function () {
 			"use strict";
 			this.audio.pause();
 			return true;
 		})	)
-	|| (commonLAg.Sound.prototype.play = commonLAg.returnTrue)
-	&& (commonLAg.Sound.prototype.pause = commonLAg.returnTrue);
-	commonLAg.Sound.audioCompat = audioCompat;
-	commonLAg.Sound.init = function (sd) { //sd : object of sounds path without extension (mp3 and ogg)
+	|| (globMeth.Sound.prototype.play = globMeth.returnTrue)
+	&& (globMeth.Sound.prototype.pause = globMeth.returnTrue);
+	globMeth.Sound.audioCompat = globMeth.audioCompat;
+	delete globMeth.audioCompat;
+	globMeth.Sound.init = function (sd) { //sd : object of sounds path without extension (mp3 and ogg)
 		"use strict";
-		commonLAg.sounds = commonLAg.sounds || [];
+		globMeth.sounds = globMeth.sounds || [];
 		for (var p in sd)
-			commonLAg.sounds[p] = new commonLAg.Sound(sd[p]);
-//to use :
-	// var sounds = { k: path.without.extension, k: etc.}
-	// commonLAg.Sound.init(sounds)
-	// commonLAg.sounds["k"].play() where "k" is the key of the sound path in object parameter sd
+			globMeth.sounds[p] = new globMeth.Sound(sd[p]);
 	}
 
 // );
