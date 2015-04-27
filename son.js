3843336445934b50185f574673d30ebe56734b3a
Interfacteur <gaetan@interfacteur.net>
2015-04-27 22:18:10 +0200

    prototype.stall, active

--- son.js
+++ son.js
@@ -1,10 +1,10 @@
-//use of sound HTML5 API - with .mp3 and .ogg
+/* Gaëtan Langhade, Interfacteur, mars-avril 2015
 
-//Gaëtan Langhade, Interfacteur, 2015 march
-
-
-/*
-	suppose jQuery
+jouer des sons en mode play vs stop
+	.mp3 et .ogg
+	avec gestion tactile et prise en compte spécificités Apple cf. http://www.ibm.com/developerworks/library/wa-ioshtml5/
+	sans les erreurs dues aux output de certains PC (win c/o Ludwik)
+suppose jQuery
 */
 
 
@@ -16,7 +16,7 @@ var globMeth = {
 // $(function () {
 	// "use strict";
 
-//Pseudo-class Sound ----------------------------------------------------------------------------
+//Fonction constructeur pour jouer les sons en mode play vs stop ----------------------------------------------------------------------------
 	globMeth.Sound = function () {
 		"use strict";
 		if (! this instanceof globMeth.Sound)
@@ -38,31 +38,35 @@ var globMeth = {
 			this.key = key;
 			this.srce = srce + globMeth.Sound.audioCompat;
 			this.audio = new Audio(this.srce);
-			this.readable = ! navigator.userAgent.toLowerCase().match(/(iphone|ipod|ipad)/) ? false : true; //loaded when playing: http://www.ibm.com/developerworks/library/wa-ioshtml5/
-			this.stall = globMeth.doNothing;
+			this.readable = ! navigator.userAgent.toLowerCase().match(/(iphone|ipod|ipad)/) ? false : true;
 			this.readdom();
 		})
+		&& (globMeth.Sound.prototype.stall = globMeth.doNothing)
+		&& (globMeth.Sound.init = -1)
 		&& (globMeth.Sound.prototype.readdom = function () {
 			"use strict";
 			$(this.audio).data("obj", this)
 			.on({
 				canplay: function () {
 					"use strict";
-					$(this).data("obj").readable = true; //to do: check play() before: and after ?
+					$(this).data("obj").readable = true; //to do : lancer play() avant et après ?
 				},
 				play: function () {
 					"use strict";
-					var t = this,
-						tob = $(t).data("obj");
-					$(t).off("play");
+					$(this).off("play");
+					if (globMeth.Sound.init === true)
+						return;
 					setTimeout(function () {
 						"use strict";
-						(tob.readable === true)
-						&& (tob.stall = function (n) {
+						if (globMeth.Sound.init === true)
+							return;
+						globMeth.Sound.init = true;
+						globMeth.Sound.prototype.stall = function (n) {
 							"use strict";
-							t.currentTime = n;
-						});
-					}, 750); //to do: how to adjust this duration?
+							this.readable === true
+							&& (this.audio.currentTime = n);
+						};
+					}, 50); //to do : comment ajuster cette durée ?
 				},
 				error: function () {
 					"use strict";
@@ -70,15 +74,19 @@ var globMeth = {
 		}	});	})
 		&& (globMeth.Sound.prototype.turnon = function () {
 			"use strict";
+			if (this.key === globMeth.Sound.active)
+				return;
 			this.readable === true
+			&& (globMeth.Sound.active = this.key)
 			&& this.audio.play();
 			return this;
 		})
 		&& (globMeth.Sound.prototype.turnoff = function () {
 			"use strict";
-			if (this.readable === true) {
+			if (globMeth.Sound.active !== false && this.readable === true) {
 				! this.audio.paused
 				&& this.audio.pause();
+				globMeth.Sound.active = false;
 				this.stall(0);
 			}
 			return this;
@@ -92,12 +100,15 @@ var globMeth = {
 	globMeth.Sound.audioCompat = globMeth.audioCompat;
 	delete globMeth.audioCompat;
 
-	globMeth.Sound.init = function (sd) { //sd : object of sounds path without extension (mp3 and ogg)
+	globMeth.Sound.init = function (sd) {
 		"use strict";
 		globMeth.sounds = globMeth.sounds || [];
 		for (var p in sd)
 			globMeth.sounds[p] = new globMeth.Sound(sd[p], p);
 	}
+
+	globMeth.Sound.active = false;
+
 
 
 // );
