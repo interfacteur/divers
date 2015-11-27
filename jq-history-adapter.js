5442434a99830e18784e78338fdb9241fb68fdc1
Interfacteur <gaetan@interfacteur.net>
2015-11-27 15:21:15 +0100

    comments

--- jq-history-adapter.js
+++ jq-history-adapter.js
@@ -1,4 +1,10 @@
-//patch pour conserver la syntaxe standard de l'API history avec history.js https://github.com/browserstate/history.js
+/* Gaëtan Langhade, Interfacteur, novembre 2015
+
+Patch pour conserver la syntaxe standard de l'API history avec history.js https://github.com/browserstate/history.js
+
+Exemple : https://github.com/interfacteur/github-api/blob/master/js/appli-dev.js
+à partir de https://github.com/interfacteur/github-api/blob/master/js/utils.js#L173 */
+
 
 history = window.History;
 history.state = history.getState().data;
