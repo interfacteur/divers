dc4fed5cf58d305783f1efb0d0a437ade05b089c
Interfacteur <gaetan@interfacteur.net>
2015-12-14 20:13:20 +0100

    proxy
    proxy without referer

--- apis-proxies/proxy.php
+++ apis-proxies/proxy.php
@@ -6,19 +6,22 @@ $targetHeaderCT = "Content-Type: text/plain; charset=utf-8";
 preg_match("/^http:\/\/www\.equatorium\.net/", $_SERVER[HTTP_REFERER], $m); //mais header("Access-Control-Allow-Origin: *");
 if (count($m) == 1) {
 	$url = explode("query=", "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
-	$context = stream_context_create(array("http" => array("header" => "User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")));
-	$target = file_get_contents($url[1], false, $context);
 
-	$targetHeader = $http_response_header;
-	foreach ($targetHeader as &$value)
-		if (strpos($value, "Content-Type:") === 0)
-			$targetHeaderCT = $value;
-}
+	if (count($url) > 1) {
+
+		$context = stream_context_create(array("http" => array("header" => "User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")));
+		$target = file_get_contents($url[1], false, $context);
+
+		$targetHeader = $http_response_header;
+		foreach ($targetHeader as &$value)
+			if (strpos($value, "Content-Type:") === 0)
+				$targetHeaderCT = $value;
+}	}
 
 header($targetHeaderCT);
 header("Cache-Control: no-cache");
 
-if (count($m) == 1) {
+if (count($url) > 1 && count($m) == 1) {
 
 	echo $target;
 
